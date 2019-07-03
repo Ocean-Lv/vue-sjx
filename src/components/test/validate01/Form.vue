18988f3e436d23e7c5b71a48da5af2a481392120
lv <lvou@btravelplus.com>
2019-07-03 17:47:01 +0800

    add validate index.vue

---
+++ src/components/test/validate01/Form.vue
@@ -0,0 +1,70 @@
+ <template>
+  <div>
+    <form>
+      <FormItem label="userName" type="text" id="userName" v-model="model.username" placeholder="username" autocomplete="off">
+        <!-- userName: <input type="text" name="userName" /> -->
+      </FormItem>
+      <FormItem label="passWord" type="text" id="passWord" v-model="model.ps" placeholder="password" autocomplete="off">
+      </FormItem>
+      <FormItem label="保存登录状态" type="checkbox">
+      </FormItem>
+      <!-- <p>password: <input type="text" name="ps" /></p>
+      <p>
+        <input type="checkbox" id="myCheck" /> <label for="myCheck">保存登录状态</label>
+      </p> -->
+      <p>
+        <button>提交表单</button>
+      </p>
+    </form>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+p {
+  margin-top: 20px;
+}
+</style>
+<script>
+import FormItem from '@/components/test/validate01/FormItem'
+
+  export default {
+    name: 'Form',
+    props:{
+      props1: {
+        type: Number,
+        default: 0
+      }
+    },
+    data () {
+      return {
+        model:{
+          username: 'tom',
+          ps: "123456"
+        },
+        rules: {
+          username: [{required: true, message: "请输入用户名"}],
+          ps: [{required: true, message: "请输入密码"}]
+        }
+      };
+    },
+
+    components: {
+      FormItem
+    },
+
+    computed: {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {},
+
+    watch: {}
+
+    }
+
+</script>
